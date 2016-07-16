0afff9d9a0994588b3b6d76ede7ebabba67ed01f
garrinar <garrinar@gmail.com>
2016-07-16 19:10:23 +0300

    install script for laravel base models and controllers

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Garrinar\Http\Controllers\BaseController;
+use Garrinar\Http\Controllers\AjaxController as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 
-class Controller extends BaseController
+class AjaxController extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
 }
---
+++ app/Models/Model.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models;
+
+use \Garrinar\Models\Eloquent\Model as BaseModel;
+
+abstract class Model extends BaseModel
+{
+
+}
